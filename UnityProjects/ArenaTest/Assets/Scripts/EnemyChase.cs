9e95ff9aa8691d180d349fe88d034924c91cfae9
Jesse Zinn <jesserzinn@gmail.com>
2025-10-27 22:20:04 -0600

    Created a new scene and also coded an AI behaviour

---
+++ UnityProjects/ArenaTest/Assets/Scripts/EnemyChase.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyChase : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+    public LayerMask whatIsGround;
+
+    private bool activated;
+    private bool newPoint;
+    private Vector3 distance;
+    private Vector3 startPos;
+    private Vector3 patrolPoint;
+
+    public float patrolRange;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        activated = false;
+        newPoint = true;
+        startPos = transform.position;
+    }
+    private void Update()
+    {
+        distance = player.transform.position - transform.position;
+        if (distance.magnitude < patrolRange) activated = true;
+        else activated = false;
+        
+        Patrol();
+    }
+    private void Chase()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+    private void Patrol()
+    {
+        if (!activated && distance.magnitude > patrolRange)
+        {
+            if (newPoint == true)
+            {
+                patrolPoint = FindPatrolPoint();
+                agent.SetDestination(patrolPoint);
+            }
+            Vector3 patrolPointDistance = patrolPoint - transform.position;
+
+            if (patrolPointDistance.magnitude <= 1f) newPoint = true;
+            else newPoint = false;
+        }
+        else
+        {
+            Chase();
+            newPoint = true;
+        }
+    }
+    private Vector3 FindPatrolPoint()
+    {
+        float xValue = UnityEngine.Random.Range(-patrolRange, patrolRange);
+        float zValue = UnityEngine.Random.Range(-patrolRange, patrolRange);
+
+        Vector3 patrolPos = new Vector3(startPos.x + xValue, startPos.y, startPos.z + zValue);
+        Ray ray = new Ray(patrolPos , - Vector3.up);
+
+        if (Physics.Raycast(ray, 5f, whatIsGround))
+        {
+            return patrolPos;
+        }
+        else return transform.position;
+    }
+}
